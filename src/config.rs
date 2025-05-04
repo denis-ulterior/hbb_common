008686c108fe48db1c6e3606bcadb3278a4839db
Denis Souza <denis.souza@ulteriorti.com.br>
2025-05-04 16:14:54 -0300

    fix

--- src/config.rs
+++ src/config.rs
@@ -99,7 +99,7 @@ const CHARS: &[char] = &[
 ];
 
 pub const RENDEZVOUS_SERVERS: &[&str] = &["vnc.ulteriorti.com.br"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RS_PUB_KEY: &str = "rN7Q1TU3Xp5MXg+sYzUgH5KazvS1RYSyGVgNcEVdJQo=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
